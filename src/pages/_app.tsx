53a18338fc0191ca0a9eea86b0ccd45ed65cf8cd
vbored <darkc9210@gmail.com>
2025-01-29 00:49:10 +0300

    null

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,18 +7,18 @@ import '@/styles/index.scss'
 import { AppProps } from 'next/app'
 import { WordPressBlocksProvider, fromThemeJson } from '@faustwp/blocks'
 import blocks from '@/wp-blocks'
-import { Questrial } from 'next/font/google'
+import { Afacad } from 'next/font/google'
 import SiteWrapperProvider from '@/container/SiteWrapperProvider'
 import { Toaster } from 'react-hot-toast'
 import NextNProgress from 'nextjs-progressbar'
 import themeJson from '@/../theme.json'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 
-const questrial = Questrial({
+const afacad = Afacad({
 	subsets: ['latin'],
 	display: 'swap',
 	weight: ['300', '400', '500', '600', '700'],
-})
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	const router = useRouter()
@@ -37,7 +37,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 					<SiteWrapperProvider {...pageProps}>
 						<style jsx global>{`
 							html {
-								font-family: ${questrial.style.fontFamily};
+								font-family: ${afacad.style.fontFamily};
 							}
 						`}</style>
 						<NextNProgress color="#818cf8" />
