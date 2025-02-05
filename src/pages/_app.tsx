2e64f415e09f0a9350932097588e735061a7367f
vbored <darkc9210@gmail.com>
2025-02-05 17:57:37 +0300

    minor code clean up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ import '@/styles/index.scss'
 import { AppProps } from 'next/app'
 import { WordPressBlocksProvider, fromThemeJson } from '@faustwp/blocks'
 import blocks from '@/wp-blocks'
-import { Afacad } from 'next/font/google'
+import { Ubuntu } from 'next/font/google'
 import SiteWrapperProvider from '@/container/SiteWrapperProvider'
 import { Toaster } from 'react-hot-toast'
 import NextNProgress from 'nextjs-progressbar'
 import themeJson from '@/../theme.json'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 
-const afacad = Afacad({
+const ubuntu = Ubuntu({
 	subsets: ['latin'],
 	display: 'swap',
 	weight: ['300', '400', '500', '600', '700'],
@@ -37,7 +37,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 					<SiteWrapperProvider {...pageProps}>
 						<style jsx global>{`
 							html {
-								font-family: ${afacad.style.fontFamily};
+								font-family: ${ubuntu.style.fontFamily};
 							}
 						`}</style>
 						<NextNProgress color="#818cf8" />
